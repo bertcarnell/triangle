8d3970c0ca820862f89b49c92e0945fbcc9c6cc9
Rob Carnell <bertcarnell@gmail.com>
2022-11-27 19:39:34 -0500

    Fixed a random error occuring in a github build

--- tests/testthat/test-mom.R
+++ tests/testthat/test-mom.R
@@ -1,7 +1,10 @@
 test_that("method of moments works", {
+  set.seed(103930)
   x <- rtriangle(20, 0, 2, 1.5)
-  mom <-triangle_mom(x)
+  mom <- triangle_mom(x)
 
   expect_equal(3, length(mom))
-  expect_true(mom[1] <= mom[3] & mom[2] >= mom[3])
+  expect_true(mom[1] <= mom[3])
+  expect_true(mom[2] >= mom[3])
+  expect_true(mom[1] <= mom[2])
 })
